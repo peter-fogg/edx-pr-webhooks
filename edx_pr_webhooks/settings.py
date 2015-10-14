ae71d0d6ddcd4758272ecb492617519e9813dd30
Peter Fogg <pfogg@edx.org>
2015-10-14 14:20:04 -0400

    Add edx_pr_webhooks to INSTALLED_APPS.

--- edx_pr_webhooks/settings.py
+++ edx_pr_webhooks/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'edx_pr_webhooks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
