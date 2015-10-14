df8fa68ec1f7806ac9c22779960964e3dbf56130
Peter Fogg <pfogg@edx.org>
2015-10-14 11:48:21 -0400

    Don't CSRF protect endpoint.

--- edx_pr_webhooks/views.py
+++ edx_pr_webhooks/views.py
@@ -3,8 +3,10 @@ Views for responding to Github API events.
 """
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def pull_request_created(request):
     print request.body
     return HttpResponse("Lookin' good.")
